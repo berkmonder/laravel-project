8d2ae983ce6820c2b01a6a3a09ebcbdbe3ce9c61
berkmonder <70864966+berkmonder@users.noreply.github.com>
2021-03-04 14:10:00 +0300

    Scaffold basic login and registration views and routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', 'ProfileController@index');
+Route::get('/', 'HomeController@index');
 
 Route::get('/profile', 'ProfileController@index');
 Route::post('/profile', 'ProfileController@store');
 
 Route::get('/member', 'MembershipController@index');
 Route::get('/consultant', 'ConsultantController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

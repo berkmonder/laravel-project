f08243bf95e109980a9d4727e1567ed0cd2df2cb
huseyntrk <huseyntrk@hotmail.com>
2021-04-18 02:27:01 +0300

    unnecessary css removed

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -10,10 +10,7 @@
         </div>
         <nav class="navbar navbar-expand-lg navbar-light p-0">
             <div class="iq-search-bar">
-                <form action="#" class="searchbox">
-                    <input type="text" class="text search-input" placeholder="Type here to search...">
-                    <a class="search-link" href="#"><i class="ri-search-line"></i></a>
-                </form>
+
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="ri-menu-3-line"></i>
@@ -30,147 +27,10 @@
                         <a class="search-toggle iq-waves-effect language-title" href="#"><img src={{ asset("images/small/flag-01.png") }} alt="img-flaf" class="img-fluid mr-1" style="height: 16px; width: 16px;" /> English <i class="ri-arrow-down-s-line"></i></a>
                         <div class="iq-sub-dropdown">
                             <a class="iq-sub-card" href="#"><img src={{ asset("images/small/flag-02.png") }} alt="img-flaf" class="img-fluid mr-2" />French</a>
-                            <a class="iq-sub-card" href="#"><img src={{ asset("images/small/flag-03.png") }} alt="img-flaf" class="img-fluid mr-2" />Spanish</a>
-                            <a class="iq-sub-card" href="#"><img src={{ asset("images/small/flag-04.png") }} alt="img-flaf" class="img-fluid mr-2" />Italian</a>
-                            <a class="iq-sub-card" href="#"><img src={{ asset("images/small/flag-05.png") }} alt="img-flaf" class="img-fluid mr-2" />German</a>
-                            <a class="iq-sub-card" href="#"><img src={{ asset("images/small/flag-06.png") }} alt="img-flaf" class="img-fluid mr-2" />Japanese</a>
                         </div>
                     </li>
                     <li class="nav-item iq-full-screen">
                         <a href="#" class="iq-waves-effect" id="btnFullscreen"><i class="ri-fullscreen-line"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="search-toggle iq-waves-effect">
-                            <i class="ri-notification-3-fill"></i>
-                            <span class="bg-danger dots"></span>
-                        </a>
-                        <div class="iq-sub-dropdown">
-                            <div class="iq-card shadow-none m-0">
-                                <div class="iq-card-body p-0 ">
-                                    <div class="bg-primary p-3">
-                                        <h5 class="mb-0 text-white">All Notifications<small class="badge  badge-light float-right pt-1">4</small></h5>
-                                    </div>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/01.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Emma Watson Bini</h6>
-                                                <small class="float-right font-size-12">Just Now</small>
-                                                <p class="mb-0">95 MB</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/02.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">New customer is join</h6>
-                                                <small class="float-right font-size-12">5 days ago</small>
-                                                <p class="mb-0">Jond Bini</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/03.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Two customer is left</h6>
-                                                <small class="float-right font-size-12">2 days ago</small>
-                                                <p class="mb-0">Jond Bini</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/04.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">New Mail from Fenny</h6>
-                                                <small class="float-right font-size-12">3 days ago</small>
-                                                <p class="mb-0">Jond Bini</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#" class="search-toggle iq-waves-effect">
-                            <i class="ri-mail-open-fill"></i>
-                            <span class="bg-primary count-mail"></span>
-                        </a>
-                        <div class="iq-sub-dropdown">
-                            <div class="iq-card shadow-none m-0">
-                                <div class="iq-card-body p-0 ">
-                                    <div class="bg-primary p-3">
-                                        <h5 class="mb-0 text-white">All Messages<small class="badge  badge-light float-right pt-1">5</small></h5>
-                                    </div>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/01.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Bini Emma Watson</h6>
-                                                <small class="float-left font-size-12">13 Jun</small>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/02.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Lorem Ipsum Watson</h6>
-                                                <small class="float-left font-size-12">20 Apr</small>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/03.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Why do we use it?</h6>
-                                                <small class="float-left font-size-12">30 Jun</small>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/04.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Variations Passages</h6>
-                                                <small class="float-left font-size-12">12 Sep</small>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="iq-sub-card" >
-                                        <div class="media align-items-center">
-                                            <div class="">
-                                                <img class="avatar-40 rounded" src={{ asset("images/user/05.jpg") }} alt="">
-                                            </div>
-                                            <div class="media-body ml-3">
-                                                <h6 class="mb-0 ">Lorem Ipsum generators</h6>
-                                                <small class="float-left font-size-12">5 Dec</small>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
                     </li>
                 </ul>
             </div>
@@ -195,7 +55,7 @@
                                         <div class="rounded iq-card-icon iq-bg-primary">
                                             <i class="ri-file-user-line"></i>
                                         </div>
-                                        <div class="media-body ml-3">   
+                                        <div class="media-body ml-3">
                                             <h6 class="mb-0 ">My Profile</h6>
                                             <p class="mb-0 font-size-12">View personal profile details.</p>
                                         </div>
@@ -209,28 +69,6 @@
                                         <div class="media-body ml-3">
                                             <h6 class="mb-0 ">Edit Profile</h6>
                                             <p class="mb-0 font-size-12">Modify your personal details.</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="account-setting.html" class="iq-sub-card iq-bg-primary-hover">
-                                    <div class="media align-items-center">
-                                        <div class="rounded iq-card-icon iq-bg-primary">
-                                            <i class="ri-account-box-line"></i>
-                                        </div>
-                                        <div class="media-body ml-3">
-                                            <h6 class="mb-0 ">Account settings</h6>
-                                            <p class="mb-0 font-size-12">Manage your account parameters.</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="privacy-setting.html" class="iq-sub-card iq-bg-primary-hover">
-                                    <div class="media align-items-center">
-                                        <div class="rounded iq-card-icon iq-bg-primary">
-                                            <i class="ri-lock-line"></i>
-                                        </div>
-                                        <div class="media-body ml-3">
-                                            <h6 class="mb-0 ">Privacy Settings</h6>
-                                            <p class="mb-0 font-size-12">Control your privacy parameters.</p>
                                         </div>
                                     </div>
                                 </a>
